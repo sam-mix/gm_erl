4dda59fbb974e84941f25208569ee85330bf464d
米鸿坤 <1045262335@qq.com>
2022-03-07 05:56:18 +0000

    x

--- apps/gm_erl/src/hello_handler.erl
+++ apps/gm_erl/src/hello_handler.erl
@@ -3,10 +3,11 @@
 -export([init/2]).
 
 init(Req, State) ->
+	Json = jsx:encode(#{library => jsx, awesome => true}),
 	Req_1 = cowboy_req:reply(
 			200,
-			#{<<"content-type">> => <<"text/plain">>},
-			<<"Hello World!">>,
+			#{<<"content-type">> => <<"application/json">>},
+			Json,
 			Req
 		),
-	{ok, Req, State}.
+	{ok, Req_1, State}.
